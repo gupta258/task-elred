6208d2af2ceb27a9b299dfb5a01b9ac88e4ecfcd
gupta258 <rachitgarg258@gmail.com>
2024-10-02 14:56:26 +0530

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import { customMetaDataGenerator } from "@/lib/customMetaDataGenerator";
 import "./globals.css";
+import Head from "next/head";
 
 const avenir = localFont({
   src: "./fonts/Avenir Next LT Pro.otf",
@@ -14,6 +15,9 @@ export const metadata = customMetaDataGenerator({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:image" content="/fullImage.png" />
+      </Head>
       <body className={`${avenir.variable} antialiased bg-white`}>
         <div className="flex items-center justify-center">
           <div className="w-full sm:w-[375px]">{children}</div>
